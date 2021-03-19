d531e67c88524a5047bbbf7e525074bad46d3d58
Ogulcan Akkir <ogulcan.akkir@asisct.com>
2021-03-19 11:48:00 +0300

    first commit

---
+++ VueJs2/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Flurl.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using VueJS.Models;
+
+namespace VueJS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly string _endPoint;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _endPoint = "https://6053592045e4b300172916de.mockapi.io/User";
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetUser()
+        {
+            var list = await _endPoint.GetJsonAsync<List<User>>();
+            return Json(list);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ VueJs2/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VueJS.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Name { get; set; }
+        public string Avatar { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string Color { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Description { get; set; }
+        public string Company { get; set; }
+        public string Title { get; set; }
+    }
+}
